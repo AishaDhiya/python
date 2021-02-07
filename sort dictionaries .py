62d59514fb4d41217f56a2d233550f7dbe18fa44
AishaDhiya <75379532+AishaDhiya@users.noreply.github.com>
2021-02-07 21:52:26 +0530

    Add files via upload

---
+++ sort dictionaries .py
@@ -0,0 +1,7 @@
+d = {'id':4, 'name' : 'abc', 'age' : 22, 'place':'xyz' 
+   print  ( " dictionary =",d) 
+arc = dict( sorted (d.items()))
+drc =dict(sorted(d.items(),reverse = true))
+print (" ascending order = " , arc)
+print("descending order = ", drc)
+	
