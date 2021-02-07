dc224bb531536975e68bbfaea904a97bf707b28c
AishaDhiya <75379532+AishaDhiya@users.noreply.github.com>
2021-02-07 21:53:55 +0530

    Add files via upload

---
+++ gcd of two numbers .py
@@ -0,0 +1,12 @@
+def compute_gcd(n1,n2):
+	if n1>n2:
+		small=n2
+	else:
+			small=n1
+			for i in range(1,small+1):
+			 def n1 % i == 0 and n2 % i == 0:
+					gcd=i
+					return gcd
+					n1=int(input("enter numer1 :"))
+					n2=int(input("enter numer2 :"))
+					print("GCD(","n1",",n2,"):",compute_gcd(n1,n2))
